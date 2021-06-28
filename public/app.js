aa2e08b47170662a32ee7232d86e20b50876ea1e
Maaz-Code <500077097@stu.upes.ac.in>
2021-06-28 23:54:26 +0530

    Added Form

--- public/app.js
+++ public/app.js
@@ -9,6 +9,8 @@ const signOutbtn = document.getElementById('signOutbtn');
 const userDetails = document.getElementById('userDetails');
 const yourGames = document.getElementById('yourGames');
 const filler = document.getElementById('filler');
+const symbol = document.getElementById('symbol');
+const yourLinks = document.getElementById('yourLinks');
 
 const provider = new firebase.auth.GoogleAuthProvider();
 signInbtn.onclick = () => auth.signInWithPopup(provider);
@@ -16,15 +18,19 @@ signOutbtn.onclick = () => auth.signOut();
 
 auth.onAuthStateChanged (user => {
     if (user) {
+        symbol.hidden = true;
         filler.hidden = true;
         yourGames.hidden = false;
+        yourLinks.hidden = false;
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
         userDetails.innerHTML = `<h1>${user.displayName}</h1> <p>${user.email}</p>`;
     } 
     else {
+        symbol.hidden = false;
         filler.hidden = false;
         yourGames.hidden = true;
+        yourLinks.hidden = true;
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
         userDetails.innerHTML = '<h1>Profile</h1> <br>';
